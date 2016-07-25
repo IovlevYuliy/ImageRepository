eea23f359623698a9f49d10108113a560f58c4d0
IovlevYuliy <iovlev2@gmail.com>
2016-07-25 13:38:25 +0600

    Add register form

--- MyProject/routes/signup.js
+++ MyProject/routes/signup.js
@@ -4,13 +4,8 @@ module.exports = function (app) {
 
     app.post('/signup', passport.authenticate('signup', {
         successRedirect: '/home',
-        failureRedirect: '/signup',
+        failureRedirect: '/',
         failureFlash : true
     }));
 
-    app.get('/signup', function (req, res) {
-        res.render('signup', {message: req.flash('message')});
-    });
-    
-    
 };
