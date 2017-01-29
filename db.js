0adffb58338c135ecdde3756b925a8ca2b87a0e9
ASKYI <Silber26@yandex.ru>
2017-01-29 15:24:24 +0700

    commit my branch pagination at the first time

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
 module.exports = {
-    //'url' : 'mongodb://admin:admin@ds042698.mlab.com:42698/userdb'
-    'url' : 'mongodb://localhost:27017/userdb'
-    };
+    'url' : 'mongodb://admin:admin@ds042698.mlab.com:42698/userdb'
+    }
--- public/javascripts/addWindow.js
+++ public/javascripts/addWindow.js
@@ -15,35 +15,72 @@ $(document).ready(function () {
         delay: 100
     });
 
+    function    OneLoadFunc(fileinput)
+    {
+        var reader = new FileReader();
+        reader.onload = (function (aImg) {
+            return function (e) {
+                $("#addmodal #size").attr('style', 'list-style:square');
+                $("#addmodal #weight").attr('style', 'list-style:square');
+                $(aImg).removeAttr("width")
+                    .removeAttr("height")
+                    .css({ width: "", height: "" });
+                aImg.attr('src', e.target.result);
+                $("#addmodal #size").text(img.width() + 'x' + img.height());
+                $("#addmodal #sz").val(img.width() + 'x' + img.height());
+                aImg.attr('width', '100%');
+                aImg.attr('height', '100%');
+            };
+        })(img);
+        reader.readAsDataURL(fileinput.files[0]);
+        var str = input.val();
+
+        $("#addmodal #newName").val(fileinput.files[0].name);
+        $("#addmodal #weight").text(fileinput.files[0].size + ' байт');
+        $("#addmodal #wt").val(fileinput.files[0].size);
+        var a = $("#addmodal #nmDefault");
+        a.val(fileinput.files[0].name);
+        a.attr('readonly', true);
+    }
+
+    function  MultiLoadFunc(fileinput) {
+
+        var reader = new FileReader();
+        reader.readAsDataURL(fileinput.files[0]);
+        var str = input.val();
+        var HtmlString = "";
+        var x = document.getElementById("filesName");
+        for (var i = 0; i < fileinput.files.length; i++) {
+            var option = document.createElement("option");
+            option.text = (i + 1).toString() + '. ' +  fileinput.files[i].name;
+            x.add(option);
+            // $("#addmodal #files").val($("#addmodal #files").value + fileinput.files[i].name.toString() + "    ");
+        }
+        $("#addmodal #filesName").show();
+        $("#addmodal #image").hide();
+        $("#addmodal #labelsize").hide();
+        $("#addmodal #sz").hide();
+        $("#addmodal #wt").hide();
+        $("#addmodal #size").hide();
+        $("#addmodal #weight").hide();
+        $("#addmodal #filename").hide();
+        $("#addmodal #nmDefault").hide();
+        $("#addmodal #weight").hide();
+
+    }
+
     //Отображение миниатюры изображения
     var img = $('#addmodal #image');
     var input = $('#addmodal #openimage');
     input.bind({
         change: function() {
-            var reader = new FileReader();
-            reader.onload = (function (aImg) {
-                return function (e) {
-                    $("#addmodal #size").attr('style', 'list-style:square');
-                    $("#addmodal #weight").attr('style', 'list-style:square');
-                    $(aImg).removeAttr("width")
-                        .removeAttr("height")
-                        .css({ width: "", height: "" });
-                    aImg.attr('src', e.target.result);
-                    $("#addmodal #size").text(img.width() + 'x' + img.height());
-                    $("#addmodal #sz").val(img.width() + 'x' + img.height());
-                    aImg.attr('width', '100%');
-                    aImg.attr('height', '100%');
-                };
-            })(img);
-            reader.readAsDataURL(this.files[0]);
-            var str = input.val();
+            if (this.files.length > 1) {
+                MultiLoadFunc(this);
+            }
+            else {
+                OneLoadFunc(this);
+            }
 
-            $("#addmodal #newName").val(this.files[0].name);
-            $("#addmodal #weight").text(this.files[0].size + ' байт');
-            $("#addmodal #wt").val(this.files[0].size);
-            var a = $("#addmodal #nmDefault");
-            a.val(this.files[0].name);
-            a.attr('readonly', true);
         }
     });
 
@@ -56,17 +93,20 @@ $(document).ready(function () {
         $("#addmodal #weight").attr('style', 'list-style:none');
         $("#addmodal #size").text("");
         $("#addmodal #weight").text("");
+
+        $("#addmodal #filesName").text("");
+        $("#addmodal #filesName").hide();
         $("#addmodal #tags").tokenfield('setTokens', ' ');
         $('#addmodal').modal('show');
     });
     
     //Обработчик для отправки изображения на сервер
     body.on('click', '#addok', function () {
-        if(!document.getElementById('addImageForm').checkValidity())
-        {
-            $("#addImageForm #dangerMsg").show();
-            return;
-        }
+        // if(!document.getElementById('addImageForm').checkValidity())
+        // {
+        //     $("#addImageForm #dangerMsg").show();
+        //     return;
+        // }
 
         var options = {
             target: '#gallery',
